914d3f2a3c8682c41e677a1edd83e436cceea377
Andre <as87321234@gmail.com>
2021-12-11 17:52:56 -0500

    Cleanup collection_gat

--- config/bin/collection_gat.py
+++ config/bin/collection_gat.py
@@ -1,51 +1,73 @@
-from bs4 import BeautifulSoup
-import datetime
-import requests
+#
+''' COLLECTION GATINEAU '''
+import sys
+from datetime import datetime
+
 import json
-from datetime import datetime, timedelta, date
+import requests
 
-now=datetime.now()
-url = 'https://api.recollect.net/api/places/B7CACAE0-E7D7-11E8-98AA-1DCEB33ECFC0/services/798/events?nomerge=1&hide=reminder_only&after=%s-%s-%s&before=%s-12-31&locale=en&include_message=email&_=1638837219' % (
-    now.year, now.month, now.day, int(now.year) + 1
+from bs4 import BeautifulSoup
+
+CURRENT_TIME: datetime = datetime.now()
+YEAR_INT = str(int(CURRENT_TIME.year) + 2)
+
+URL = f'https://api.recollect.net/api/places/B8CACAE0-E7D7-11E8-98AA-1DCEB33ECFC0/services/' \
+      f'798/events?nomerge=1&hide=reminder_only&after=' \
+      f'{CURRENT_TIME.year}-{CURRENT_TIME.month}-{CURRENT_TIME.day}&before={YEAR_INT}-01-01' \
+      f'&locale=en&include_message=email&_=1638837220'
+
+URL = 'https://api.recollect.net/api/places/B7CACAE0-E7D7-11E8-98AA-1DCEB33ECFC0/services/798/events?nomerge=1&hide=reminder_only&after=%s-%s-%s&before=%s-12-31&locale=en&include_message=email&_=1638837219' % (
+    '2021', '12', '10', int('2021') + 1
 )
-bearer="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmYzZmY2M4Y2M0MjY0MDZlODA0ZTkzYjE5MmI3MTVjNCIsImlhdCI6MTYzODg0ODc3MCwiZXhwIjoxOTU0MjA4NzcwfQ.ee0px9b4bnU1QTInWdtoSl4XYwaXiLtGYTyso8eP_QU"
-# print(url)
-page = requests.get(url)
+print(URL)
 
-if page.status_code != 200:
-    exit(1)
+BEARER = "bearer eyJ1eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmYzZmY2M4Y2M0MjY0MDZlODA0Z" \
+         "TkzYjE5MmI3MTVjNCIsImlhdCI6MTYzODg0ODc3MCwiZXhwIjoxOTU0MjA4NzcwfQ.ee0px9b4bnU1QTInWdto" \
+         "Sl4XYwaXiLtGYTyso8eP_QU"
 
-soup = BeautifulSoup(page.text, 'html.parser')
-site_json=json.loads(soup.text)
+BEARER="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmYzZmY2M4Y2M0MjY0MDZlODA0ZTkzYjE5MmI3MTVjNCIsImlhdCI6MTYzODg0ODc3MCwiZXhwIjoxOTU0MjA4NzcwfQ.ee0px9b4bnU1QTInWdtoSl4XYwaXiLtGYTyso8eP_QU"
+
+PAGE = requests.get(URL)
+
+if PAGE.status_code != 200:
+    sys.exit(0)
+
+SOUP = BeautifulSoup(PAGE.text, 'html.parser')
+site_json = json.loads(SOUP.text)
+
+# print(json.dumps(site_json,  indent=2))
 
 out = {}
 
 for e in site_json["events"]:
-    name=e['flags'][0]['name']
-    name=name.replace("\"","").lower()
-    date=e['day']
-    date=date.replace("\"","")
-    event_time=datetime(int(date.split('-')[0]), int(date.split('-')[1]), int(date.split('-')[2]))
 
-    if event_time > now :
+    name = e['flags'][0]['name']
+    name = name.replace("\"", "").lower()
+    a_date = e['day']
+    a_date = a_date.replace("\"", "")
+    event_time = datetime(
+        int(a_date.split('-')[0]), int(a_date.split('-')[1]), int(a_date.split('-')[2]))
+
+    if event_time > datetime.now():
 
         try:
-            if out[name] >  date:
-               out[name]=date
+            if out[name] > a_date:
+                out[name] = a_date
         except KeyError:
-           out[name]=date
+            out[name] = a_date
 
 
 # Process collect events
 for e in out:
-    data='{"state":"%s","attributes":{"friendly_name":"sensor.collection_gatineau_%s","device_class":"timestamp"}}' %(out[e],e)
+    PAYLOAD = "{\"state\":\"{out[e]}\",\"attributes\":{\"friendly_name\":\"" \
+        "sensor.collection_gatineau_{e}\",\"device_class\":\"timestamp\"}}"
 
-    req=requests.post('http://localhost:8123/api/states/sensor.collection_gatineau_%s' % e,
-                  headers = {'authorization': bearer ,
-                            'content-type': 'application/json'
-                            },
-                  data = data
-                  )
-    print(req.content)
-    #print(json.dumps(out))
-
+    req = requests.post(
+        "https://has.pointtomap.com/api/states/sensor.collection_gatineau_{e}",
+            headers={'authorization': BEARER,
+                        'content-type': 'application/json'
+                        },
+            data=PAYLOAD
+            )
+    # print(req.content)
+print(json.dumps(out))
