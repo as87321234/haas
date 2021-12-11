1db3d37797ed28f33366267573a7cca6565e2874
Andre <as87321234@gmail.com>
2021-12-11 18:17:23 -0500

    Remove dead code

--- config/bin/collection_gat.py
+++ config/bin/collection_gat.py
@@ -1,4 +1,3 @@
-#
 ''' COLLECTION GATINEAU '''
 import sys
 from datetime import datetime
@@ -9,23 +8,17 @@ import requests
 from bs4 import BeautifulSoup
 
 CURRENT_TIME: datetime = datetime.now()
-YEAR_INT = str(int(CURRENT_TIME.year) + 2)
+YEAR_INT = str(int(CURRENT_TIME.year) + 1)
 
-URL = f'https://api.recollect.net/api/places/B8CACAE0-E7D7-11E8-98AA-1DCEB33ECFC0/services/' \
+URL = f'https://api.recollect.net/api/places/B7CACAE0-E7D7-11E8-98AA-1DCEB33ECFC0/services/' \
       f'798/events?nomerge=1&hide=reminder_only&after=' \
       f'{CURRENT_TIME.year}-{CURRENT_TIME.month}-{CURRENT_TIME.day}&before={YEAR_INT}-01-01' \
       f'&locale=en&include_message=email&_=1638837220'
 
-URL = 'https://api.recollect.net/api/places/B7CACAE0-E7D7-11E8-98AA-1DCEB33ECFC0/services/798/events?nomerge=1&hide=reminder_only&after=%s-%s-%s&before=%s-12-31&locale=en&include_message=email&_=1638837219' % (
-    '2021', '12', '10', int('2021') + 1
-)
-print(URL)
 
 BEARER = "bearer eyJ1eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmYzZmY2M4Y2M0MjY0MDZlODA0Z" \
          "TkzYjE5MmI3MTVjNCIsImlhdCI6MTYzODg0ODc3MCwiZXhwIjoxOTU0MjA4NzcwfQ.ee0px9b4bnU1QTInWdto" \
          "Sl4XYwaXiLtGYTyso8eP_QU"
-
-BEARER="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmYzZmY2M4Y2M0MjY0MDZlODA0ZTkzYjE5MmI3MTVjNCIsImlhdCI6MTYzODg0ODc3MCwiZXhwIjoxOTU0MjA4NzcwfQ.ee0px9b4bnU1QTInWdtoSl4XYwaXiLtGYTyso8eP_QU"
 
 PAGE = requests.get(URL)
 
@@ -59,6 +52,7 @@ for e in site_json["events"]:
 
 # Process collect events
 for e in out:
+
     PAYLOAD = "{\"state\":\"{out[e]}\",\"attributes\":{\"friendly_name\":\"" \
         "sensor.collection_gatineau_{e}\",\"device_class\":\"timestamp\"}}"
 
@@ -70,4 +64,4 @@ for e in out:
             data=PAYLOAD
             )
     # print(req.content)
-print(json.dumps(out))
+#print(json.dumps(out))
